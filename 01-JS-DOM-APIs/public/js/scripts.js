ce74d7a1f7b3a80560b2fae0e415e751cf1c7846
unknown <guidovargamartinez123@gmail.com>
2018-10-17 19:33:32 -0300

    exercise 2

--- 01-JS-DOM-APIs/public/js/scripts.js
+++ 01-JS-DOM-APIs/public/js/scripts.js
@@ -2,3 +2,13 @@
 window.onload= ()=>{
 	document.getElementById('hidden-section').classList.toggle('fade');
 }
+
+//Exercise 2
+
+document.getElementById('button').onclick =function(){
+	buttonClicked();
+}
+
+const buttonClicked = () =>{
+	alert('The button has been clicked');
+}
