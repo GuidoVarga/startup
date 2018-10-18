330de55aca91beeab3ff70540cb8f1c8b3d95f35
guido <guidovargamartinez123@gmail.com>
2018-10-18 11:44:35 -0300

    Data fetching: 3.1, 3.2 and 3.3 + sintax style fixed

--- 01-JS-DOM-APIs/public/js/scripts.js
+++ 01-JS-DOM-APIs/public/js/scripts.js
@@ -1,14 +1,47 @@
-//Exercise 1
-window.onload= ()=>{
+window.onload = () => {
 	document.getElementById('hidden-section').classList.toggle('fade');
-}
+} //I'm not sure if there should be a semicolon here
 
-//Exercise 2
-
-document.getElementById('button').onclick =function(){
-	buttonClicked();
-}
-
-const buttonClicked = () =>{
+const buttonClicked = () => {
 	alert('The button has been clicked');
-}
+};
+
+document.getElementById('button-jokes').onclick = function () {
+	getJoke();
+} //I'm not sure if there should be a semicolon here
+
+const getRequest = (url,callback) => {
+
+	let request = window.ActiveXObject ?
+	new ActiveXObject('Microsoft.XMLHTTP') :
+	new XMLHttpRequest;
+
+	request.onload = () => {
+		if(request.status == 200){
+			callback(request, request.status);
+		}
+		else{
+			alert('error:' + request.status);
+		}
+	};
+	request.onerror = () => {
+		alert('error:' + request.status);
+	}
+
+	request.open('GET',url,true);
+	request.send(null);
+};
+
+const getJoke = () => {
+	getRequest('http://api.icndb.com/jokes/random', (data) => {
+
+		let response = JSON.parse(data.response);
+		let joke = response['value']['joke'];
+		let article = document.getElementById('joke-article');
+		article.innerHTML = "";
+		let h4 = document.createElement('h4');
+		let content = document.createTextNode(joke);
+		h4.appendChild(content);
+		article.appendChild(h4);
+	});
+};
