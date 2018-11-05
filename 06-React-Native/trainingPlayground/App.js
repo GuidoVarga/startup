5aa4cd86f5921741b62bbf2693847f2c44efdafc
guido <guidovargamartinez123@gmail.com>
2018-11-05 11:32:08 -0300

    React Native: Core components: 2

--- 06-React-Native/trainingPlayground/App.js
+++ 06-React-Native/trainingPlayground/App.js
@@ -11,22 +11,20 @@ import {Platform, StyleSheet, Text, View} from 'react-native';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
+  android: 'Double tap R on your keyboard to reload,\n' +
+  'Shake or press menu button for dev menu',
 });
 
-type Props = {};
+type
+Props = {};
 export default class App extends Component<Props> {
   render() {
     return (
-    <View style={styles.view1}>
-
-
-      <View style={styles.view2}>
-     </View>
-    <View style={styles.view3}>
-    </View>
+     <View style={styles.view1}>
+       <View style={styles.view2}>
+       </View>
+       <View style={styles.view3}>
+       </View>
      </View>
     );
   }
@@ -34,7 +32,7 @@ export default class App extends Component<Props> {
 
 const styles = StyleSheet.create({
   view1: {
-    flex:1,
+    flex: 1,
     borderColor: 'black',
     borderRadius: 10,
     borderWidth: 1,
