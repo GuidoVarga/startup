0126b1c10a9d537722d90a205b1ae5f6995de1e8
guido <guidovargamartinez123@gmail.com>
2018-11-06 15:11:50 -0300

    React Native: Styling and layout: 1 + folder structure

--- 06-React-Native/trainingPlayground/App.js
+++ 06-React-Native/trainingPlayground/App.js
@@ -5,80 +5,17 @@
  * @format
  * @flow
  */
-
 import React, {Component} from 'react';
 import {StyleSheet, Text, View, ScrollView, Image, Dimensions} from 'react-native';
-
-const width = Dimensions.get('window').width;
-const viewHeight = 400;
+import ScreenT1 from './topic1/ScreenT1';
+import ScreenT2 from './topic2/ScreenT2';
 
 type
 Props = {};
 export default class App extends Component<Props> {
   render() {
     return (
-     <ScrollView styles={styles.scrollView}>
-       <View style={styles.view1}>
-         <Text style={styles.textTitle}>Hello</Text>
-         <Image style={styles.img1}
-                source={{uri: 'https://images-na.ssl-images-amazon.com/images/I/614R93DFDFL.jpg'}}
-         />
-       </View>
-       <View style={styles.view2}>
-         <Image style={styles.img2}
-                resizeMode={'cover'}
-                source={require('./public/images/gaara-vs-sasuke.png')}
-         />
-       </View>
-       <View style={styles.view3}>
-         <Text style={{fontWeight: 'bold'}}>
-           Hello
-           <Text style={{color: 'red'}}>
-             there!
-           </Text>
-         </Text>
-       </View>
-     </ScrollView>
+     <ScreenT2/>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  view1: {
-    height: viewHeight,
-    borderColor: 'black',
-    borderRadius: 10,
-    borderWidth: 1,
-    alignItems: 'center',
-    backgroundColor: 'red',
-  },
-  view2: {
-    height: viewHeight,
-    borderColor: 'violet',
-    backgroundColor: 'orange',
-    alignItems: 'center'
-  },
-  view3: {
-    height: viewHeight,
-    borderColor: 'black',
-    borderRadius: 10,
-    borderWidth: 1,
-    backgroundColor: 'yellow',
-    alignItems: 'center'
-  },
-  text: {
-    fontSize: 17,
-  },
-  textTitle: {
-    fontSize: 40,
-    fontWeight: 'bold'
-  },
-  img1: {
-    width: 300,
-    height: 300,
-  },
-  img2: {
-    height: viewHeight,
-    width: width
-  }
-});
---
+++ 06-React-Native/trainingPlayground/topic2/styles.js
@@ -0,0 +1,35 @@
+/**
+ * Created by Guido on 6/11/2018.
+ */
+import {StyleSheet} from 'react-native';
+
+const styles = StyleSheet.create({
+
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    backgroundColor: '#1190FF'
+  },
+  box: {
+    margin: 7,
+    marginBottom: 10,
+  },
+  box1: {
+    height: 100,
+    width: 100,
+    backgroundColor: 'red',
+  },
+  box2: {
+    height: 150,
+    width: 150,
+    backgroundColor: 'purple',
+  },
+  box3: {
+    height: 200,
+    width: 200,
+    borderColor: 'black',
+    backgroundColor: 'white',
+    alignItems: 'center'
+  },
+});
+export default styles;
