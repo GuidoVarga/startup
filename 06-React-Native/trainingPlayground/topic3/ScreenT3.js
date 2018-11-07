0df11719be8b48e6554522f1e6c10b57662d1498
guido <guidovargamartinez123@gmail.com>
2018-11-07 11:17:24 -0300

    React Native: Handling Events: 2

--- 06-React-Native/trainingPlayground/topic3/ScreenT3.js
+++ 06-React-Native/trainingPlayground/topic3/ScreenT3.js
@@ -2,34 +2,49 @@
  * Created by Guido on 7/11/2018.
  */
 import React, {Component} from 'react';
-import {Text, View, ScrollView, Image,TextInput} from 'react-native';
+import {Text, View, TextInput, TouchableOpacity} from 'react-native';
+import styles from './styles';
 
 class ScreenT3 extends Component {
 
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
       text: ''
     };
     this.onChangeText = this.onChangeText.bind(this);
+    this.onPressTouchable = this.onPressTouchable.bind(this);
   }
 
-  onChangeText(text){
+  onChangeText(text) {
     this.setState({
       text: text
     });
-    console.log(this.state.text);
+  }
+
+  onPressTouchable() {
+    this.setState({
+      text: ''
+    });
   }
 
   render() {
     return (
-       <View>
-        <TextInput
-         placeholder={'Write something!'}
-         value = {this.state.text}
-         onChangeText={this.onChangeText}
-        />
-       </View>
+     <View style={styles.container}>
+       <TextInput
+        style={styles.textInput}
+        placeholder={'Write something here!'}
+        value={this.state.text}
+        onChangeText={this.onChangeText}
+       />
+       <TouchableOpacity onPress={this.onPressTouchable}>
+         <View style={styles.button}>
+           <Text style={styles.buttonText}>
+             Clear text
+           </Text>
+         </View>
+       </TouchableOpacity>
+     </View>
     );
   }
 }
---
+++ 06-React-Native/trainingPlayground/topic3/styles.js
@@ -0,0 +1,28 @@
+/**
+ * Created by Guido on 7/11/2018.
+ */
+import {StyleSheet} from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+   alignItems:'center',
+    paddingTop: 10,
+  },
+  button: {
+    width: 150,
+    height:50,
+    borderRadius: 10,
+    backgroundColor: '#1190FF',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  textInput: {
+    marginBottom: 10
+  },
+  buttonText: {
+    fontSize: 16,
+    color: 'white',
+    fontWeight: 'bold'
+  }
+});
+export default styles;
