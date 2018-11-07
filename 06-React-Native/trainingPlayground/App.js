088670800d5d6a414d28bbac637752ec2099ec94
guido <guidovargamartinez123@gmail.com>
2018-11-07 16:25:59 -0300

    React Native: Navigation: 4

--- 06-React-Native/trainingPlayground/App.js
+++ 06-React-Native/trainingPlayground/App.js
@@ -9,12 +9,25 @@ import React, {Component} from 'react';
 import  HomeScreen from './topic4-6/HomeScreen';
 import  SecondaryScreen from './topic4-6/SecondaryScreen';
 import {createBottomTabNavigator, createTabNavigator} from 'react-navigation';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 const App = createBottomTabNavigator({
-   Home: HomeScreen,
-   Secondary: SecondaryScreen,
+   Forms: HomeScreen,
+   Other: SecondaryScreen,
  },
  {
+   navigationOptions: ({navigation}) => ({
+     tabBarIcon: ({tintColor}) => {
+       const {routeName} = navigation.state;
+       let iconName;
+       if (routeName === 'Forms') {
+         iconName = 'ios-home';
+       } else if (routeName === 'Other') {
+         iconName = 'ios-apps';
+       }
+       return <Ionicons name={iconName} size={27} color={tintColor}/>;
+     },
+   }),
    tabBarOptions: {
      activeTintColor: 'red',
      inactiveTintColor: 'gray',
